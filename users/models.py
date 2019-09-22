43fb991b3f89a629a982cdbe4f6f58acb8c908b9
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-09-22 18:11:33 +0530

    custom user model created and registered on admin site

--- users/models.py
+++ users/models.py
@@ -3,14 +3,71 @@ from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
 )
 
-
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
         """
-        Create and saves user with given email
+        Creates and saves user with given email
         and password
         """
         if not email:
             raise ValueError("Users must have an email address")
 
-        user = self.model
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password):
+        """
+        Creates and save superuser with given email
+        and password
+        """
+        user = self.create_user(
+            email,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def get_username(self):
+        return self.email
+
+    def has_perm(sefl, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+    
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`"
+        # Simplest possible answer: Yes, always
+        return True
+    
+    @property
+    def is_staff(self):
+        "Is the user a memeber of staff"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+    
+    
+        
